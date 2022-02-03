e44caec2249e1af38fb41443659659002b542adb
Jim Anderton <janderton@indecon.com>
2022-02-03 14:20:27 -0800

    k8s job creation attempt

--- src/main/java/gov/epa/bencloud/server/jobs/K8sApiExample.java
+++ src/main/java/gov/epa/bencloud/server/jobs/K8sApiExample.java
@@ -1,18 +1,20 @@
 package gov.epa.bencloud.server.jobs;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.Configuration;
-import io.kubernetes.client.openapi.apis.BatchApi;
-import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.apis.BatchV1Api;
+import io.kubernetes.client.openapi.models.V1EnvVar;
+import io.kubernetes.client.openapi.models.V1EnvVarBuilder;
 import io.kubernetes.client.openapi.models.V1Job;
-
-import io.kubernetes.client.openapi.models.V1Pod;
-import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.openapi.models.V1JobBuilder;
 import io.kubernetes.client.util.ClientBuilder;
-import io.kubernetes.client.util.Config;
 import spark.Request;
 import spark.Response;
 
@@ -24,24 +26,20 @@ public class K8sApiExample {
 
     public static Object runTest(Request req, Response res) {
     	try {
-	    	//ApiClient client  = Config.defaultClient();
 	    	ApiClient client = ClientBuilder.cluster().build();
 	    	
 	    	Configuration.setDefaultApiClient(client);
 	    	
 	    	client.setDebugging(true);
-	    	
-	    	logger.debug("k8s base path: " + client.getBasePath());
-	    	
-	        CoreV1Api api = new CoreV1Api();
 
-	        V1PodList list = api.listNamespacedPod("benmap-dev", "true", null, null, null, null, null, null, null, null, null);
-	        for (V1Pod item : list.getItems()) {
-	          logger.debug("pod: " + item.getMetadata().getName());
-	        }
-	    	
-	    	/*
+	    	logger.debug("k8s base path: " + client.getBasePath());
+
 	    	BatchV1Api api = new BatchV1Api(client);
+	    	List<V1EnvVar> envVariables = new ArrayList<V1EnvVar>();
+	    	V1EnvVar envVar = new V1EnvVar();
+	    	envVar.setName("REDEPLOY_META");
+	    	envVar.setValue("$CI_JOB_ID-$CI_COMMIT_SHA");
+	    	envVariables.add(envVar);
 	    	
 	    	logger.debug("api: " + api.toString());
 	    	V1Job body = new V1JobBuilder()
@@ -49,21 +47,36 @@ public class K8sApiExample {
 	    	  .withKind("Job")
 	    	  .withNewMetadata()
 	    	    .withNamespace("benmap-dev")
-	    	    .withName("bencloud-job")
+	    	    .withGenerateName("bencloud-job-")
+	    	    .withAnnotations(
+	    	    	Map.of(
+	    	    		"app.gitlab.com/app", "${CI_PROJECT_PATH_SLUG}", 
+	    	    		"app.gitlab.com/env", "${CI_ENVIRONMENT_SLUG}"
+	    	    	))
 	    	    .endMetadata()
 	    	  .withNewSpec()
 	    	    .withNewTemplate()
 	    	      .withNewMetadata()
-	    	        .addToLabels("name", "hif-analysis")
+	    	        .addToLabels("name", "bencloud-taskrunner")
+		    	    .withAnnotations(
+		    	    	Map.of(
+		    	    		"app.gitlab.com/app", "${CI_PROJECT_PATH_SLUG}", 
+		    	    		"app.gitlab.com/env", "${CI_ENVIRONMENT_SLUG}"
+		    	    	))
 	    	        .endMetadata()
 	    	      .editOrNewSpec()
 	    	        .addNewContainer()
-	    	          .withName("main")
-	    	          .withImage("task-runner")
+	    	          .withName("taskrunner")
+	    	          .withImage("registry.epa.gov/benmap/bencloudserver/bencloudtaskrunner:$CI_COMMIT_SHORT_SHA")
+	    	          .withImagePullPolicy("Always")
+	    	          .withEnv(envVariables)
 	    	          .addNewCommand("task")
 	    	          .addNewArg("--id")
 	    	          .addNewArg("ABC123")
-	    	          .endContainer()
+	    	        .endContainer()
+	    	        .addNewImagePullSecret()
+	    	        	.withName("glcr-auth")
+	    	        .endImagePullSecret()
 	    	        .withRestartPolicy("Never")
 	    	        .endSpec()
 	    	      .endTemplate()
@@ -75,7 +88,7 @@ public class K8sApiExample {
 	    	V1Job createdJob = api.createNamespacedJob("benmap-dev", body, "true", null, null);
 	    	
 	    	logger.debug("Job status: " + createdJob.getStatus());
-	    	*/
+
 	    	return true;
 	    	
     	} catch (Exception e) {
