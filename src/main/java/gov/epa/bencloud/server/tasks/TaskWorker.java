c915a9673798fe56e16cb07ba34c603a5dccd1da
Jim Anderton <janderton@indecon.com>
2022-03-09 08:04:24 -0800

    Extend timeout for unresponsive tasks

--- src/main/java/gov/epa/bencloud/server/tasks/TaskWorker.java
+++ src/main/java/gov/epa/bencloud/server/tasks/TaskWorker.java
@@ -31,7 +31,7 @@ public class TaskWorker {
 
 	public static int maxTaskWorkers = 0;
 
-	private static final int UNRESPONSIVE_TASK_WORKER_TIME_IN_MINUTES = 3; 
+	private static final int UNRESPONSIVE_TASK_WORKER_TIME_IN_MINUTES = 5; 
 
 	static {
 		maxTaskWorkers = Integer.parseInt(ApplicationUtil.getProperty("max.task.workers"));
