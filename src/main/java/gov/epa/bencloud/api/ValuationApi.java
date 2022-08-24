ae89b0963f065d33d2c878150095b67cdabe050f
Jim Anderton <janderton@indecon.com>
2022-08-24 13:12:27 -0700

    Remove percentiles from valuation results

--- src/main/java/gov/epa/bencloud/api/ValuationApi.java
+++ src/main/java/gov/epa/bencloud/api/ValuationApi.java
@@ -198,7 +198,7 @@ public class ValuationApi {
 					}
 				}
 				//Remove percentiles by keeping all other fields
-				vfRecordsClean = vfRecords.into(vfRecords.fields(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21));
+				vfRecordsClean = vfRecords.into(vfRecords.fields(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
 			
 		} catch (DataAccessException e) {
 			e.printStackTrace();
@@ -358,7 +358,7 @@ public class ValuationApi {
 							}
 						}
 						//Remove percentiles by keeping all other fields
-						vfRecordsClean = vfRecords.into(vfRecords.fields(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21));
+						vfRecordsClean = vfRecords.into(vfRecords.fields(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
 			} catch (DataAccessException e) {
 				e.printStackTrace();
 				response.status(400);
