241a1652011a6d33a90a7144d8f0e991a6398a0e
Jim Anderton <janderton@indecon.com>
2022-08-31 08:00:57 -0700

    Updating AQ surfaces. Preparing for new db release.

---
+++ db/patch-008.sql
@@ -0,0 +1,5 @@
+-- In this release, we replaced the entire database as part of the final staging and production release before SAB review
+-- New CSAPR AQ surfaces from EPA
+-- General review and cleanup
+
+-- db version is 8
