6e1694548a3dc8edd4240af86f37963519623a20
ADriver <adrian.driver@noaa.gov>
2023-02-07 14:46:54 -0800

    Update batch task configuration object and related objects

--- src/main/java/gov/epa/bencloud/api/model/BatchTaskConfig.java
+++ src/main/java/gov/epa/bencloud/api/model/BatchTaskConfig.java
@@ -7,7 +7,6 @@ import static gov.epa.bencloud.server.database.jooq.data.Tables.POPULATION_DATAS
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Arrays;
 
 import org.jooq.JSON;
 import org.jooq.Record10;
@@ -41,7 +40,7 @@ public class BatchTaskConfig {
 	public String pollutantName;
 	public Boolean preserveLegacyBehavior = true;
 	public List<Scenario> aqScenarios = new ArrayList<Scenario>();
-	public List<HifGroup> hifGroups = new ArrayList<HifGroup>();
+	public List<BatchHIFGroup> batchHifGroups = new ArrayList<BatchHIFGroup>();
 	
 	/*
 	 * Default constructor
@@ -77,7 +76,7 @@ public class BatchTaskConfig {
 			JsonNode functionGroups = params.get("hif_function_groups");
 
 			for (JsonNode functionGroup : functionGroups) {
-				this.hifGroups.add(new HifGroup(functionGroup));
+				this.batchHifGroups.add(new BatchHIFGroup(functionGroup));
 			}
 
 		} catch (JsonMappingException e) {
@@ -216,8 +215,8 @@ public class BatchTaskConfig {
 		b.append("HEALTH IMPACT GROUPS\n\n");
 		b.append("Health Effect Groups Analyzed:\n");
 
-		for(HifGroup hifGroup : hifGroups) {
-			b.append(hifGroup.toString());
+		for(BatchHIFGroup batchHifGroup : batchHifGroups) {
+			b.append(batchHifGroup.toString());
 		}
 		
 		b.append("ADVANCED SETTINGS\n\n");
@@ -226,152 +225,4 @@ public class BatchTaskConfig {
 		return b.toString();
 	}
 	
-}
-
-
-class Scenario {
-    public Integer id = 0;
-    public String name;
-    public List<PopConfig> popConfigs = new ArrayList<PopConfig>();
-
-	/**
-	 * Creates an object from the task parameter object
-	 * @param scenario
-	 */
-	public Scenario(JsonNode scenario) {
-		id = scenario.has("post_policy_aq_id") ? scenario.get("post_policy_aq_id").asInt() : null;
-		name = scenario.has("post_policy_aq") ? scenario.get("post_policy_aq").asText() : null;
-		JsonNode popConfigsJson = scenario.has("population_configs") ? scenario.get("population_configs") : null;
-		if(popConfigsJson != null) {
-			for(JsonNode popConfig : popConfigsJson) {
-				popConfigs.add(new PopConfig(popConfig));
-			}
-		}
-	}
-
-		/**
-	 * 
-	 * @param userProfile
-	 * @return string representation of the aq scenario
-	 */
-	public String toString(Optional<UserProfile> userProfile) {
-		StringBuilder b = new StringBuilder();
-		b.append("Scenario id: ").append(id).append("\n");
-		b.append("Population Configurations:\n");
-		for(int i = 0; i < popConfigs.size(); i++) {
-			b.append(popConfigs.get(i).toString(userProfile, i+1)).append("\n");
-		}
-		return b.toString();
-	}
-}
-
-class PopConfig {
-    public Integer popYear = null;
-    public List<HifConfig> hifConfigs = new ArrayList<HifConfig>();
-
-	/**
-	 * Creates an object from the task parameter object
-	 * @param popConfig
-	 */
-	public PopConfig(JsonNode popConfig) {
-		popYear = popConfig.has("population_year") ? popConfig.get("population_year").asInt() : null;
-		JsonNode hifConfigsJson = popConfig.has("hif_configs") ? popConfig.get("hif_configs") : null;
-		if(hifConfigsJson != null) {
-			for(JsonNode hifConfig : hifConfigsJson) {
-				hifConfigs.add(new HifConfig(hifConfig));
-			}
-		}
-	}
-
-	/**
-	 * 
-	 * @param userProfile
-	 * @return string representation of the population configuration
-	 */
-	public String toString(Optional<UserProfile> userProfile, Integer count) {
-		StringBuilder b = new StringBuilder();
-		b.append("Population Configuration (").append(count).append("):\n");
-		b.append("Population year: ").append(popYear).append("\n");
-		b.append("HIF Configurations:\n");
-		for(int i = 0; i < hifConfigs.size(); i++) {
-			b.append(hifConfigs.get(i).toString(userProfile, i+1)).append("\n");
-		}
-		return b.toString();
-	}
-}
-
-class HifConfig {
-    public Integer hifInstanceId = null;
-    public Integer incidenceYear = null;
-    public List<Integer> prevalenceYears = new ArrayList<Integer>();
-
-	/**
-	 * Creates an object from the task parameter object
-	 * @param hifConfig
-	 */
-	public HifConfig(JsonNode hifConfig) {
-		hifInstanceId = hifConfig.has("hif_instance_id") ? hifConfig.get("hif_instance_id").asInt() : null;
-		incidenceYear = hifConfig.has("incidence_year") ? hifConfig.get("incidence_year").asInt() : null;
-		String[] currentPrevalence = hifConfig.has("prevalence_years") ? hifConfig.get("prevalence_years").asText().split(",") : null;
-		if(currentPrevalence != null) {	
-			for(String year : currentPrevalence) {
-				prevalenceYears.add(Integer.valueOf(year));
-			}
-		}
-	}
-
-	/**
-	 * 
-	 * @param userProfile
-	 * @return string representation of the HIF configuration
-	 */
-	public String toString(Optional<UserProfile> userProfile, Integer count) {
-		StringBuilder b = new StringBuilder();
-		b.append("HIF Configuration (").append(count).append("):\n");
-		b.append("HIF instance id: ").append(hifInstanceId).append("\n");
-		b.append("Incidence year: ").append(incidenceYear).append("\n");
-		b.append("Prevalance years: ").append(Arrays.toString(prevalenceYears.toArray())).append("\n");
-		return b.toString();
-	}
-}
-
-class HifGroup {
-	public Integer id = null;
-	public String name = null;
-	public List<HIFConfig> hifs = new ArrayList<HIFConfig>();
-
-	/**
-	 * Creates an object from the task parameter object
-	 * @param hifGroup
-	 */
-	public HifGroup(JsonNode hifGroup) {
-		id = hifGroup.has("id") ? hifGroup.get("id").asInt() : null;
-		name = hifGroup.has("name") ? hifGroup.get("name").asText() : null;
-		JsonNode hifFunctions = hifGroup.has("hif_functions") ? hifGroup.get("hif_functions") : null;
-		if(hifFunctions != null) {
-			for(JsonNode function : hifFunctions) {
-				hifs.add(new HIFConfig(function));
-			}
-		}
-	}
-
-	/**
-	 * 
-	 * @param userProfile
-	 * @return string representation of the HIF group
-	 */
-	public String toString(Optional<UserProfile> userProfile) {
-		StringBuilder b = new StringBuilder();
-		b.append("Health impact group name: ").append(name).append("\n");
-		b.append("Functions Selected: ").append(hifs.size()).append("\n\n");
-		b.append("Health impact functions:\n");
-		//hifs are sorted by endpoint group and endpoint by default
-		for(int i=0; i < hifs.size(); i++) {
-			HIFConfig hif = hifs.get(i);
-			b.append("Function ").append(i+1).append(":\n");
-			b.append(hif.toString()).append("\n");
-		}
-		b.append("\n");
-		return b.toString();
-	}
 }
