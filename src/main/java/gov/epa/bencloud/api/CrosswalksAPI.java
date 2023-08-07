afb72b9b9ddb3a93a762dee9136a570ee5723d3b
IEC\BWu <BWU@indecon.com>
2023-08-07 11:51:38 -0400

    forwards and backwards crosswalks

--- src/main/java/gov/epa/bencloud/api/CrosswalksAPI.java
+++ src/main/java/gov/epa/bencloud/api/CrosswalksAPI.java
@@ -6,11 +6,15 @@ import gov.epa.bencloud.server.database.JooqUtil;
 import gov.epa.bencloud.server.database.jooq.data.tables.records.CrosswalkDatasetRecord;
 import gov.epa.bencloud.server.database.jooq.data.tables.records.CrosswalkEntryRecord;
 import org.jooq.impl.DSL;
+import org.jooq.DSLContext;
 import org.jooq.InsertOnDuplicateStep;
 import org.jooq.InsertValuesStep7;
+import org.jooq.InsertValuesStep8;
+
 import org.jooq.Record1;
 import org.jooq.Record2;
 import org.jooq.Record7;
+
 import org.jooq.SelectConditionStep;
 
 
@@ -20,7 +24,8 @@ public class CrosswalksAPI{
     
     public static void calculateCrosswalks(int grid_id1, int grid_id2){ 
         
-        CrosswalkDatasetRecord crosswalkDatasetRecord = DSL.using(JooqUtil.getJooqConfiguration())
+        DSLContext dslContext = DSL.using(JooqUtil.getJooqConfiguration());
+        CrosswalkDatasetRecord crosswalkDatasetRecord = dslContext
 			.insertInto(CROSSWALK_DATASET
 					, CROSSWALK_DATASET.SOURCE_GRID_ID
 					, CROSSWALK_DATASET.TARGET_GRID_ID
@@ -29,6 +34,7 @@ public class CrosswalksAPI{
 			.returning(CROSSWALK_DATASET.ID)
 			.fetchOne();
 
+        Integer forwardCrosswalkID = crosswalkDatasetRecord.value1();
 
         Record1<String> g1 = DSL.using(JooqUtil.getJooqConfiguration()).select(GRID_DEFINITION.NAME).from(GRID_DEFINITION).where(GRID_DEFINITION.ID.eq(grid_id1)).fetchOne();
         Record1<String> g2 = DSL.using(JooqUtil.getJooqConfiguration()).select(GRID_DEFINITION.NAME).from(GRID_DEFINITION).where(GRID_DEFINITION.ID.eq(grid_id2)).fetchOne();
@@ -36,7 +42,7 @@ public class CrosswalksAPI{
         String g1Name = g1.value1();
         String g2Name = g2.value1();
 
-       SelectConditionStep<Record7<Integer, Integer, Long, Integer, Integer, Long, Double>> selectQuery = DSL.using(JooqUtil.getJooqConfiguration())
+       SelectConditionStep<Record7<Integer, Integer, Long, Integer, Integer, Long, Double>> selectQuery = dslContext
         .select(
                 DSL.field(g1Name + ".col", Integer.class).as("source_col"),
                 DSL.field(g1Name + ".row", Integer.class).as("source_row"),
@@ -50,8 +56,9 @@ public class CrosswalksAPI{
         .where("st_intersects(g1.geom, g2.geom)")
         .and("st_area(st_intersection(g1.geom, g2.geom)) / st_area(g1.geom) > 0.00000001");
 
-        InsertOnDuplicateStep<CrosswalkEntryRecord> crosswalkEntryRecord = DSL.using(JooqUtil.getJooqConfiguration())
+        InsertOnDuplicateStep<CrosswalkEntryRecord> crosswalkEntryRecord = dslContext
             .insertInto(CROSSWALK_ENTRY
+                    , CROSSWALK_ENTRY.CROSSWALK_ID
                     , CROSSWALK_ENTRY.SOURCE_COL
                     , CROSSWALK_ENTRY.SOURCE_ROW
                     , CROSSWALK_ENTRY.SOURCE_GRID_CELL_ID
@@ -60,7 +67,17 @@ public class CrosswalksAPI{
                     , CROSSWALK_ENTRY.TARGET_GRID_CELL_ID
                     , CROSSWALK_ENTRY.PERCENTAGE
             )
-            .select(selectQuery);
+            .select(
+            DSL.value(forwardCrosswalkID),
+            selectQuery.field("source_col"),
+            selectQuery.field("source_row"),
+            selectQuery.field("source_grid_cell_id"),
+            selectQuery.field("target_col"),
+            selectQuery.field("target_row"),
+            selectQuery.field("target_grid_cell_id"),
+            selectQuery.field("percentage")
+        )
+        .from(selectQuery);
             
                      
     }
