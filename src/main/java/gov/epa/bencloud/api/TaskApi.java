5d8bff9f253cc86afdb18f1e2799f58cc0fee8b8
YingziYang <yyang@indecon.com>
2023-09-21 15:23:01 -0400

    Try so solve BCD-301

--- src/main/java/gov/epa/bencloud/api/TaskApi.java
+++ src/main/java/gov/epa/bencloud/api/TaskApi.java
@@ -550,7 +550,7 @@ public class TaskApi {
 		}
 				
 		if(maxTasks != 0 && taskCount >= maxTasks) {
-			return CoreApi.getErrorResponse(request, response, 401, "You have reached the maximum of " + maxTasks + " task scenarios allowed per user. Please delete existing task results before submitting new tasks.");
+			return CoreApi.getSuccessResponse(request, response, 200, "You have reached the maximum of " + maxTasks + " task scenarios allowed per user. Please delete existing task results before submitting new tasks.");
 		}
 	
 		
