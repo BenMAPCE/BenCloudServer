441f78226e0d4bc71403f805c8cebeae1e5aa11b
Jim Anderton <janderton@indecon.com>
2025-02-05 12:09:18 -0800

    Updated sql to set db version to 38

--- db/patch-038.sql
+++ db/patch-038.sql
@@ -1,4 +1,5 @@
 -- Add or replace index to crosswalk_entry table.
+UPDATE "data".settings SET value_int=38 WHERE "key"='version';
 
 DROP INDEX IF EXISTS data.crosswalk_entry_crosswalk_id_id_idx;
 CREATE INDEX crosswalk_entry_crosswalk_id_id_idx ON data.crosswalk_entry USING btree (crosswalk_id);
