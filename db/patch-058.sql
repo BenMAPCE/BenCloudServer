fd534a053b982c08608c9ed2a78b3e6306e209fc
YingziYang <yyang@indecon.com>
2025-06-05 18:45:25 -0400

    First step of BWD-152: mark 2010 grids as archive.

---
+++ db/patch-058.sql
@@ -0,0 +1,9 @@
+/**** Mark 2010 grid definitions are archive ****/
+
+UPDATE "data".settings SET value_int=58 WHERE "key"='version';
+
+ALTER TABLE "data".grid_definition ADD archive int2 DEFAULT 0 NULL;
+update grid_definition gd set archive = 1 where id in (18,19,20);
+
+VACUUM (VERBOSE, ANALYZE) "data".grid_definition;
+
