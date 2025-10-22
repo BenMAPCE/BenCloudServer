058c85ba88a2bda751c964f476c56ca1c24e15fb
Jim Anderton <janderton@indecon.com>
2025-10-22 08:30:40 -0700

    Fix filename

--- db/patch-074-draft.sql
+++
@@ -1,7 +0,0 @@
-/**** Add support for AQ import ****/
-
-UPDATE data.settings SET value_int=74 WHERE "key"='version';
-
-CREATE UNIQUE INDEX population_value_pop_value ON data.population_value USING btree (pop_entry_id, grid_cell_id, pop_value);
-
-ALTER TABLE "data".air_quality_layer ADD task_log json NULL;
