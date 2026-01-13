fa38ffad5aabe06e843a6d9462c248fffde390e2
YingziYang <yyang@indecon.com>
2026-01-13 12:08:34 -0500

    Updated patch-090 to only update existing endpoints under default Mortality endpoint group, and to handle when there are more than one existing "Mortality, Respiratory, infant".

--- db/patch-090.sql
+++ db/patch-090.sql
@@ -4,29 +4,39 @@
 
 DO $$
 DECLARE
-    v_old_endpoint_id bigint;
     v_new_endpoint_id bigint;
 BEGIN
 
 	UPDATE "data".settings SET value_int=90 where "key"='version';
 
-    -- Find existing health effect
-    SELECT id INTO v_old_endpoint_id
-    FROM "data".endpoint
-    WHERE name ILIKE 'Mortality, Respiratory, infant';
+	CREATE TEMPORARY TABLE tmp_old_endpoint (
+	    endpoint_id int4
+	);	
 
-    -- Insert new health effect
+    -- Find existing health effect in default Mortality group
+    INSERT INTO tmp_old_endpoint(endpoint_id)
+    SELECT id
+    FROM "data".endpoint
+    WHERE name ILIKE 'Mortality, Respiratory, infant' and endpoint_group_id = 12 and name <> 'Mortality, Respiratory, infant';
+
+    -- Insert new health effect if not already exists
     INSERT INTO "data".endpoint (endpoint_group_id, name, display_name)	
-    VALUES (12, 'Mortality, Respiratory, infant', 'Mortality, Respiratory')
+    SELECT 12, 'Mortality, Respiratory, infant', 'Mortality, Respiratory'
+	WHERE NOT EXISTS (SELECT 1 FROM "data".endpoint WHERE endpoint_group_id = 12 AND name = 'Mortality, Respiratory, infant')
     RETURNING id INTO v_new_endpoint_id;
+	
+	--Get v_new_endpoint_id if already exists
+	IF v_new_endpoint_id IS NULL THEN
+		SELECT id INTO v_new_endpoint_id FROM "data".endpoint WHERE endpoint_group_id = 12 AND name = 'Mortality, Respiratory, infant';
+	END IF;
 
     -- If an existing health effect record existed, update references
-    IF v_old_endpoint_id IS NOT NULL THEN
-        UPDATE "data".health_impact_function SET endpoint_id = v_new_endpoint_id WHERE endpoint_id = v_old_endpoint_id;
-        UPDATE "data".valuation_function SET endpoint_id = v_new_endpoint_id WHERE endpoint_id = v_old_endpoint_id;
-        UPDATE "data".incidence_entry SET endpoint_id = v_new_endpoint_id WHERE endpoint_id = v_old_endpoint_id;
+    IF EXISTS (SELECT 1 FROM tmp_old_endpoint) THEN
+        UPDATE "data".health_impact_function SET endpoint_id = v_new_endpoint_id WHERE endpoint_id IN (SELECT endpoint_id FROM tmp_old_endpoint) ;
+        UPDATE "data".valuation_function SET endpoint_id = v_new_endpoint_id WHERE endpoint_id IN (SELECT endpoint_id FROM tmp_old_endpoint);
+        UPDATE "data".incidence_entry SET endpoint_id = v_new_endpoint_id WHERE endpoint_id IN (SELECT endpoint_id FROM tmp_old_endpoint);
 
-        DELETE FROM "data".endpoint WHERE id = v_old_endpoint_id;
+        DELETE FROM "data".endpoint WHERE id IN (SELECT endpoint_id FROM tmp_old_endpoint);
     END IF;
 
 
